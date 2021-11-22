2ca4e5d5789bc9e9fd417f6520f9ac1c01f95822
User <User@DESKTOP-5KTKN3M>
2021-11-22 20:20:34 +1000

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вычесление радиуса  и площади круга
+            double a = Math.PI;
+            double secondNumbe;
+            secondNumbe = 30;
+            double radius = secondNumbe / (2 * a);
+            double areaCirele = a * (radius * radius);
+            Console.WriteLine("Радиус=");
+        Console.WriteLine(radius);
+            Console.WriteLine("Периметр=");
+            Console.WriteLine(areaCirele);
+            Console.ReadKey();
+        }
+    }
+}
